24e36d1290d02cd9c0c1849f53c56d83d583da31
supersheep <supersheepxjy@gmail.com>
2013-06-04 09:57:54 +0800

    give version_map in callback

--- test/cortex-install.js
+++ test/cortex-install.js
@@ -1,6 +1,8 @@
 var Installer = require("../index");
 
-var installer = new Installer();
+var installer = new Installer({
+    registry:"http://registry.npm.dp"
+});
 
 
 // installer.installModule("ajax","0.0.x",function(err,json){
@@ -8,7 +10,7 @@ var installer = new Installer();
 //     console.log(json);
 // });
 
-installer.install(["ajax@0.0.x","authbox@0.0.0"],function(err,json){
+installer.install(["ajax@0.0.x","authbox@0.0.0","ajax@0.0.2"],function(err,json,version_map){
     if(err){console.log(err);return;}
-    console.log(json);
+    console.log(json,version_map);
 })
